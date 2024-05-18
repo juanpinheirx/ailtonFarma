a9326c9a26860a5ef1caf866365fe0486580218e
juanpinheirx <jroliveirapinheiro@gmail.com>
2024-05-18 10:44:51 -0300

    feitos os botões de navegação

--- src/components/subcomponents/Header.tsx
+++ src/components/subcomponents/Header.tsx
@@ -1,12 +1,20 @@
 import './Header.css';
 import logo from '../../assets/LOGO.svg'; // Import the logo using ES6 import syntax
+import { Button } from '../utils/Button';
 
 export const Header = () => {
   return (
     <>
       <div id='header'>
-        <img src={logo} alt='bandeira-do-brasil' id='bandeira' />
+        <div id='banner'>
+          <img src={logo} alt='bandeira-do-brasil' id='bandeira' />
+        </div>
         <h1 id='titulo'>FARMÁCIA AILTON FARMA</h1>
+        <div id='button'>
+          <Button />
+          <Button />
+          <Button />
+        </div>
       </div>
     </>
   );
---
+++ src/components/utils/Button.tsx
@@ -0,0 +1,13 @@
+import './Button.css';
+
+export const Button = () => {
+  return (
+    <div id='button'>
+      <div id='buttons2'>
+        <a className='btn btn-primary' href='#' role='button'>
+          Link
+        </a>
+      </div>
+    </div>
+  );
+};
