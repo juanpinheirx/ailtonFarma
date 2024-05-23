766e1ce25b270a9cee6aeeaf26325321d7f59cea
juanpinheirx <jroliveirapinheiro@gmail.com>
2024-05-22 22:44:21 -0300

    ajustado alguns botoes da header

--- src/components/subcomponents/Header.tsx
+++ src/components/subcomponents/Header.tsx
@@ -10,15 +10,23 @@ export const Header = () => {
         </div>
         <h1 id='titulo'>FARMÁCIA AILTON FARMA</h1>
         <div id='button'>
+          <a href='http://ailtonfarma.vercel.app' rel='noopener noreferrer'>
+            <div id='buttons2'>
+              <button type='button' className='btn btn-light' name='Link'>
+                INÍCIO
+              </button>
+            </div>
+          </a>
           <div id='buttons2'>
-            <button type='button' className='btn btn-light' name='Link'>
-              INÍCIO
-            </button>
-          </div>
-          <div id='buttons2'>
-            <button type='button' className='btn btn-light' name='Link'>
-              COMPRAR
-            </button>
+            <a
+              href='https://api.whatsapp.com/send/?phone=5582987357475&text&type=phone_number&app_absent=0'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button type='button' className='btn btn-light' name='Link'>
+                COMPRAR
+              </button>
+            </a>
           </div>{' '}
           <div id='buttons2'>
             <button type='button' className='btn btn-light' name='Link'>
